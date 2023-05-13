51bdc8ee65e9a7e02662ee70c8cba56e9764ddcb
pruthivithejan <pruthivithejan.code@gmail.com>
2023-05-13 13:14:47 +0530

    Siparana 2.0 - Hero & Nav 1.0

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,18 +6,18 @@ const Hero = () => {
     <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
       <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
         <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
-          <h1 className="text-5xl md:text-6xl lg:text-7xl three-d text-emerald-500 dark:text-emerald-500">
-            Easily determine <br /> text similarity.
+          <h1 className="text-5xl md:text-6xl font-bold text-center lg:text-7xl lg:text-left three-d text-emerald-500 dark:text-emerald-500">
+            සිප් අරණ <br />{" "}
+            <span className="dark:text-slate-300 text-slate-800">
+              අධ්‍යාපන සත්කාරය
+            </span>
           </h1>
+          <h3 className="text-3xl md:text-4xl font-semibold text-center lg:text-left three-d text-slate-700 dark:text-slate-300">
+            කලාප අධ්‍යාපන කාර්යාලය - අම්පාර
+          </h3>
           <p className="lg:text-left max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center">
-            With the Text Similarity API, you can easily determine the
-            similarity between two pieces of text with a free{" "}
-            <Link
-              href="/login"
-              className="underline underline-offset-2 text-black dark:text-emerald-500"
-            >
-              API key
-            </Link>
+            දැනුම තුළින් යහපත් සමාජයක් උදෙසා, අම්පාර අධ්‍යාපන කලාපයේ පමණක් නොව
+            මුලු සිරිලක දැයේ දරුවන්හට පිදෙන අධ්‍යාපන සත්කාරයයි !
           </p>
           <div className="relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
             <Image
