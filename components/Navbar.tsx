092809f7946962164200e9bdd1ff7397d9a7d6db
pruthivithejan <pruthivithejan.code@gmail.com>
2023-05-14 13:42:12 +0530

    Switched to Shadcn UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,19 @@
+import { ComboboxDemo } from "./Search";
+import { ModeToggle } from "./ModeToggle";
+
 const Navbar = () => {
   return (
-    <nav className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
+    <nav className="fixed backdrop-blur-sm bg-white/75 dark:bg-background/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full px-4 md:px-8 flex justify-between items-center">
         <a href="/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-emerald-400 text-2xl font-semibold whitespace-nowrap">
+          <img src="/siparana.svg" className="h-8 mr-3" alt="Siparana Logo" />
+          <span className="self-center text-emerald-500 text-2xl font-semibold whitespace-nowrap">
             සිප් අරණ
           </span>
         </a>
         <div className="flex md:order-2">
-          <div className="relative block ">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-slate-400"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Search icon</span>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block w-full p-2 pl-10 text-sm text-slate-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-emerald-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-400 dark:focus:border-emerald-400"
-              placeholder="සොයන්න..."
-            />
-          </div>
-          <button
-            data-collapse-toggle="navbar-search"
-            type="button"
-            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-search"
-            aria-expanded="false"
-          ></button>
+          <ComboboxDemo />
+          <ModeToggle />
         </div>
       </div>
     </nav>
